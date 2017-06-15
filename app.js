7057dfeedca10ac4adc87edbaa734427dcf32c2e
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-06-15 01:34:50 -0400

    added styling

--- app.js
+++ app.js
@@ -3,24 +3,25 @@ $(document).ready(function() {
 $('#search').click(function(){
 
   var searchTerm = $('#searchTerm').val();
-  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
-
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
+console.log(url);
 $.ajax({
   type: "GET",
-  url:url,
+  url: url,
+  contentType: "application/json; charset=utf-8",
   async:false,
   dataType: 'json',
-  success: function(data){
+  success: function(data, textStatus, jqXHR){
 // console.log(data[1][0])
 // console.log(data[2][0])
 // console.log(data[3][0])
 // event.preventDefault();
-
+$('#header').hide();
 $('#output').html('');
 for(var i = 0;i<data[1].length;i++){
-$('#output').append("<li><a href = "+data[3][i] + "</a><p>"+ data[2][i]+"</p></li>");
+$('#output').append("<div><div class='btn'><a href = "+data[3][i]+ "><h2>"+ data[1][i]+"</h2>" +  "<p>" + data[2][i]+ "</p></a></div></div>");
 }
-$("#searchTerm").val('');
+$('#searchTerm').val('');
   },
   error: function(errorMessage){
     alert("Error")
@@ -30,9 +31,9 @@ $("#searchTerm").val('');
 });
 
 });
-$("#searchTerm").keypress(function(e){
+$('#searchTerm').keypress(function(e){
   if(e.which==13){
-    $("#search").click();
+    $('#search').click();
   }
 });
 
